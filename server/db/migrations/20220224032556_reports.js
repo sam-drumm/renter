3ffa95c57ec4de19ecce14872a88dcb3085f67b0
Isabella Edith Stevens Dampney <isabella.dampney@gmail.com>
2022-02-24 17:42:59 +1300

    migrations for properties, reports

---
+++ server/db/migrations/20220224032556_reports.js
@@ -0,0 +1,36 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('reports', table => {
+    table.increments('id')
+    table.integer('property_id').references('properties.id')
+    table.integer('user_id').references('users.id')
+    table.string('address').references('properties.address')
+    table.integer('rooms_1')
+    table.integer('rooms_2')
+    table.integer('rent_total')
+    table.string('rent_utilities')
+    table.integer('year_1')
+    table.integer('year_2')
+    table.string('property_managed_by')
+    table.string('rent_increase_frequency')
+    table.integer('ave_increase')
+    table.boolean('heat_pump')
+    table.boolean('insulation')
+    table.boolean('fridge')
+    table.boolean('curtains')
+    table.boolean('oven')
+    table.boolean('smoke_alarm')
+    table.boolean('pets_ok')
+    table.boolean('fire_extinguisher')
+    table.boolean('smoking_ok')
+    table.boolean('subletting_allowed')
+    table.integer('responsive_repairs')
+    table.integer('rental_management')
+    table.string('repairs')
+    table.integer('sufficient_notice')
+    table.integer('rate_relationship')
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('reports')
+}

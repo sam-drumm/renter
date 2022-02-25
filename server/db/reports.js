a82bfec0f09a1616e504e42e219238d38995dd1f
sam-drumm <samueldrumm@gmail.com>
2022-02-25 15:28:09 +1300

    Add DB function to getReportById

--- server/db/reports.js
+++ server/db/reports.js
@@ -5,6 +5,41 @@ function getReports (db = connection) {
     .select()
 }
 
+function getReportsById (id, db = connection) {
+  return db('reports')
+    .join('properties', 'reports.id', 'properties.id')
+    .where('report.id', id)
+    .select(
+      'properties.id as propertyId',
+      'properties.address as address',
+      'reports.rooms_1 as rooms1',
+      'reports.rooms_2 as rooms2',
+      'reports.rent_total as rentTotal',
+      'reports.rent_utilities as rentUtilities',
+      'reports.year_1 as year1',
+      'reports.year_2 as year2',
+      'reports.property_managed_by as propertyManagedBy',
+      'reports.rent_increase_frequency as rentIncreaseFrequency',
+      'reports.ave_increase as aveIncrease',
+      'reports.heat_pump as heatPump',
+      'reports.insulation as insulation',
+      'reports.fridge as fridge',
+      'reports.curtains as curtains',
+      'reports.oven as oven',
+      'reports.smoke_alarm as smokeAlarm',
+      'reports.fire_extinguisher as fireExtinguisher',
+      'reports.pets_ok as petsOk',
+      'reports.smoking_ok as smokingOk',
+      'reports.subletting_allowed as sublettingAllowed',
+      'reports.responsive_repairs as responsiveRepairs',
+      'reports.rental_management as rentalManagement',
+      'reports.repairs as repairs',
+      'reports.sufficient_notice as sufficientNotice',
+      'reports.rate_relationship as rateRelationship'
+    )
+    .first()
+}
+
 /// address from properties
 
 function addReport (newReport, db = connection) {
@@ -43,5 +78,6 @@ function addReport (newReport, db = connection) {
 
 module.exports = {
   getReports,
+  getReportsById,
   addReport
 }

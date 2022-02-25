3071175c7a89376925f4a0f1ca04d4a9b45e9d2e
sam-drumm <samueldrumm@gmail.com>
2022-02-25 14:41:39 +1300

    correct css issue

--- server/db/seeds/users.js
+++ server/db/seeds/users.js
@@ -5,11 +5,13 @@ exports.seed = function (knex) {
         {
           id: 1,
           auth0_id: 'auth0|61414f84d35ac900717bc280',
+          nickname: 'penelope',
           email: 'kelmarna@email.nz'
         },
         {
           id: 2,
           auth0_id: 'auth0|61414f84d35ac900717bc282',
+          nickname: 'john',
           email: 'sammy@example.com'
         }
       ])

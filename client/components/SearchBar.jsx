c78db6fe7f1960daf54cb3d3d40978577191e054
ahmad <eng.ahmed.anwar@gmail.com>
2022-02-26 10:55:28 +1300

    fix lint errors

--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -3,32 +3,31 @@ import SearchIcon from '@mui/icons-material/Search'
 import CloseIcon from '@mui/icons-material/Close'
 
 export function SearchBar ({ placeholder, data }) {
+  const [filteredData, setFilteredData] = useState([])
+  const [wordEntered, setWordEntered] = useState('')
 
-const [filteredData, setFilteredData ] = useState([])
-const [wordEntered, setWordEntered] = useState("")
+  const handleFilter = (event) => {
+    const searchWord = event.target.value
+    // address typed in by the user in search bar
+    setWordEntered(searchWord)
 
-const handleFilter = (event) => {
-  const searchWord = event.target.value
-  //address typed in by the user in search bar
-  setWordEntered(searchWord)
-
-   const newFilter = data.filter((value) => {
-    return value.address.toLowerCase().includes(searchWord.toLowerCase())
-  //filter data for addresses that match the one typed by the user
-  })
-  if (searchWord === "") {
-    setFilteredData([])
-  } else {
-    setFilteredData(newFilter)
+    const newFilter = data.filter((value) => {
+      return value.address.toLowerCase().includes(searchWord.toLowerCase())
+      // filter data for addresses that match the one typed by the user
+    })
+    if (searchWord === '') {
+      setFilteredData([])
+    } else {
+      setFilteredData(newFilter)
+    }
   }
-}
   const clearInput = () => {
     setFilteredData([])
-    setWordEntered("")
+    setWordEntered('')
   }
 
-  function handleClick(value) {
-  console.log(value)
+  function handleClick (value) {
+    console.log(value)
   }
 
   return (
@@ -36,21 +35,21 @@ const handleFilter = (event) => {
       <div className='searchInputs'>
         <input type='text' placeholder={placeholder} value={wordEntered} onChange={handleFilter}/>
         <div className='searchIcon'>
-          {filteredData.length === 0 
-          ? (<SearchIcon />)
-          : (<CloseIcon id="clearBtn" onClick={clearInput}/>)
+          {filteredData.length === 0
+            ? (<SearchIcon />)
+            : (<CloseIcon id="clearBtn" onClick={clearInput}/>)
           }
-          </div>
+        </div>
       </div>
-    {filteredData.length != 0 && (
-      <div className='dataResult'>
-        {filteredData.slice(0, 5).map((value, key) => {
-          return (<a className='dataItem' href="#" key={value.id} onClick={()=> handleClick(value) }><p>{value.address}</p></a>
-        )
-        //used slice so that when the api fetches addresses, it will show the first 5 matches
-        })}
-      </div>
-    )}
+      {filteredData.length != 0 && (
+        <div className='dataResult'>
+          {filteredData.slice(0, 5).map((value, key) => {
+            return (<a className='dataItem' href="#" key={value.id} onClick={() => handleClick(value) }><p>{value.address}</p></a>
+            )
+            // used slice so that when the api fetches addresses, it will show the first 5 matches
+          })}
+        </div>
+      )}
     </div>
   )
 }

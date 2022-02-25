bc83de36973d18d9362802facef51a4cee4d3ab1
Nikhita <k.nikhita@gmail.com>
2022-02-26 08:51:14 +1300

    add auto complete functionality to search bar

--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -1,19 +1,52 @@
-import React from 'react'
-import HouseIcon from '@mui/icons-material/House'
+import React, { useState } from 'react'
+import SearchIcon from '@mui/icons-material/Search'
+import CloseIcon from '@mui/icons-material/Close'
 
 export function SearchBar ({ placeholder, data }) {
- 
+
+const [filteredData, setFilteredData ] = useState([])
+const [wordEntered, setWordEntered] = useState("")
+
+const handleFilter = (event) => {
+  const searchWord = event.target.value
+  //address typed in by the user in search bar
+  setWordEntered(searchWord)
+
+   const newFilter = data.filter((value) => {
+    return value.address.toLowerCase().includes(searchWord.toLowerCase())
+  //filter data for addresses that match the one typed by the user
+  })
+  if (searchWord === "") {
+    setFilteredData([])
+  } else {
+    setFilteredData(newFilter)
+  }
+}
+  const clearInput = () => {
+    setFilteredData([])
+    setWordEntered("")
+  }
+
   return (
     <div className='search'>
       <div className='searchInputs'>
-        <input type='text' placeholder={placeholder}/>
-        <div className='searchIcon'> <HouseIcon /></div>
+        <input type='text' placeholder={placeholder} value={wordEntered} onChange={handleFilter}/>
+        <div className='searchIcon'>
+          {filteredData.length === 0 
+          ? (<SearchIcon />)
+          : (<CloseIcon id="clearBtn" onClick={clearInput}/>)
+          }
+          </div>
       </div>
+    {filteredData.length != 0 && (
       <div className='dataResult'>
-        {data.map((value, key) => {
-          return <a className='dataItem' href="#"><p>{value.address}</p></a>
+        {filteredData.slice(0, 5).map((value, key) => {
+          return (<a className='dataItem' href="#" key={value.id}><p>{value.address}</p></a>
+        )
+        //used slice so that when the api fetches addresses, it will show the first 5 matches
         })}
       </div>
+    )}
     </div>
   )
 }

9aa9f1b0f4ebb5a98ba95d0db50299b268efda94
Ningyi <nicole005213@gmail.com>
2022-02-27 22:00:41 +1300

    add dark mode switch

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { cacheUser } from '../auth0-utils'
 import { Home } from './Home'
@@ -9,21 +9,37 @@ import Registration from './Registration'
 import RentalForm from './RentalForm'
 import Users from './Users'
 import DataResponsePage from './DataResponsePage'
+import { Button, Paper, Switch } from '@material-ui/core'
+import { ThemeProvider, createTheme } from '@material-ui/core/styles'
 
 function App () {
   cacheUser(useAuth0)
 
-  return (
-    <div className='app'>
-      <Route exact path='/' component={Nav} />
-      <Route exact path='/' component={Home} />
-      <Route exact path='/' component={PingRoutes} />
-      <Route exact path='/users' component={Users} />
-      <Route path='/register' component={Registration} />
-      <Route path='/reports/:address' component={DataResponsePage} />
-      <Route path='/rentalform' component={RentalForm} />
-    </div>
+  const [darkMode, setDarkMode] = useState(false)
 
+  const theme = createTheme({
+    palette: {
+      type: darkMode ? 'dark' : 'light'
+    }
+  })
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Paper style={{ height: '100vh' }}>
+        <div className='app'>
+          <Route exact path='/' component={Nav} />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/' component={PingRoutes} />
+          <Route exact path='/users' component={Users} />
+          <Route path='/register' component={Registration} />
+          <Route path='/reports/:address' component={DataResponsePage} />
+          <Route path='/rentalform' component={RentalForm} />
+          <Button>
+            <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)}/>
+          </Button>
+        </div>
+      </Paper>
+    </ThemeProvider>
   )
 }
 

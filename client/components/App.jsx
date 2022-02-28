94ec8456d4b5bd658f7e5a0a866a609b3b993baf
sam-drumm <samueldrumm@gmail.com>
2022-02-28 13:49:01 +1300

    Cleaning up

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,27 +1,27 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { cacheUser } from '../auth0-utils'
-import Home from './Home'
 import { Route, Routes } from 'react-router-dom'
 import Header from './Header'
+import SearchBar from './SearchBar'
 import PingRoutes from './PingRoutes'
 import Registration from './Registration'
 import RentalForm from './RentalForm'
 import Users from './Users'
-import { Button, Paper, Switch } from '@material-ui/core'
-import { ThemeProvider, createTheme } from '@material-ui/core/styles'
+// import { Button, Paper, Switch } from '@material-ui/core'
+// import { ThemeProvider, createTheme } from '@material-ui/core/styles'
 import DataResponseRedo from './DataResponseRedo'
 
 function App () {
   cacheUser(useAuth0)
 
-  const [darkMode, setDarkMode] = useState(false)
+  // const [darkMode, setDarkMode] = useState(false)
 
-  const theme = createTheme({
-    palette: {
-      type: darkMode ? 'dark' : 'light'
-    }
-  })
+  // const theme = createTheme({
+  //   palette: {
+  //     type: darkMode ? 'dark' : 'light'
+  //   }
+  // })
 
   return (
     <>
@@ -34,14 +34,14 @@ function App () {
           <Route path='/register' element={<Registration/>} />
           <Route path='/reports/:address' element={<DataResponseRedo/>} />
           <Route path='/rentalform' element={<RentalForm/>} />
-
         </Routes>
-
       </main>
+
+      {/* <Paper/>
+      <ThemeProvider/> */}
+
     </>
 
-      </Paper>
-    </ThemeProvider>
   )
 }
 

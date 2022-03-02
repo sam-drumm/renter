1c8c2b0ce49884aa930c70a34d9a3c98f2338bd7
Nikhita <k.nikhita@gmail.com>
2022-03-02 21:31:20 +1300

    fixed logo styling on sign in page - applied vanilla css

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Nav from './Nav'
-import { Box, Image } from '@chakra-ui/react'
 
 export default function Header () {
   return (
     <>
     <Link to="/">
-    <Box boxSize='150px' float='left' ml={10}>
-    <Image src='./images/RenterFinal-removebg-preview.png' alt="renterlogo"/>
-    </Box>
+    <img src='./images/RenterFinal-removebg-preview.png' alt="renterlogo" className='logo'/>
     </Link>
     <Nav />
     </>
